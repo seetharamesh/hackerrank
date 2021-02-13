28d841f7f65e69f47d5e62821c069f13c5cf8447
seetharamesh <seetharamesh@hotmail.com>
2021-02-12 22:31:52 -0500

    loops

---
+++ src/hackkerrank/Java_Loops_1.java
@@ -0,0 +1,16 @@
+package hackkerrank;
+import java.util.Scanner;
+public class Java_Loops_1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		 int N = scanner.nextInt();
+	        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	        for(int i = 1 ; i <= 10; i++)
+	        System.out.println(N + " x " + i + " = " + N*i);
+
+	        scanner.close();
+
+	}
+
+}
