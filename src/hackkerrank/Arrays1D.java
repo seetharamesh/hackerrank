044875322ed08d97d2229190aa060b7cac7b7f33
seetharamesh <seetharamesh@hotmail.com>
2021-03-22 19:35:53 -0400

    arrays one dimensional

---
+++ src/hackkerrank/Arrays1D.java
@@ -0,0 +1,23 @@
+package hackkerrank;
+
+import java.util.*;
+
+public class Arrays1D {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] a = new int[n];
+        for(int i = 0; i < a.length; i++){
+            int val = scan.nextInt();
+            a[i] = val;
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
