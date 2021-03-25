c08b3d2b27c66c9cca78fc9f67f840d0f32bddaa
seetharamesh <seetharamesh@hotmail.com>
2021-03-24 20:03:23 -0400

    matching brackets

---
+++ src/hackkerrank/StackExample.java
@@ -0,0 +1,44 @@
+package hackkerrank;
+
+import java.util.*;
+class StackExample{
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);	
+		//initialize an empty stack
+		while (sc.hasNext()) {
+			String input=sc.next();
+			//System.out.println(input);
+			//System.out.println(input.length());
+			Stack<Character> st = new Stack<>();
+			char ch = ' ';
+			for(int i = 0; i < input.length(); i++) {
+				ch = input.charAt(i);
+				if(!st.isEmpty())//if stack is not empty
+				{
+					//get each char from input and peek in stack if it has closing braces. 
+					//If so pop the input out so that it indicates a matching exist and return true
+					switch(ch) {
+					case '}': if(st.peek() == '{') {
+						st.pop();
+					}
+					break;
+					
+					case ']': if(st.peek() == '[') st.pop();
+					break;
+					
+					case ')': if(st.peek() == '(') st.pop();
+					break;
+					
+					default: st.push(ch);
+					
+					}
+				} else st.push(ch);
+			}
+			System.out.println(st.isEmpty());//if anything exists in stack that means brackets are not matching so it was not popped out
+		}
+		sc.close();
+		
+	}
+}
