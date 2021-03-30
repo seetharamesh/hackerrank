782d652d70266ea626b45c4517657939476ba718
seetharamesh <seetharamesh@hotmail.com>
2021-03-29 22:49:51 -0400

    Variable args

---
+++ src/hackkerrank/VarargsExample.java
@@ -0,0 +1,66 @@
+package hackkerrank;
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+//Write your code here
+class Add{
+	public void add(int ...num) {//the 3 dots specifies that this method can take any number of arguments
+		int sum = 0;
+		String seperator="";
+		for(int i: num) {
+			sum += i;
+			System.out.print(seperator + i);
+			seperator = "+";
+		}
+		System.out.println("="+sum);
+	}
+}
+
+
+public class VarargsExample {
+
+    public static void main(String[] args) {
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+
+}
