44e71c9918c3c273bc74c60dd5cafe471bc0a9a5
seetharamesh <seetharamesh@hotmail.com>
2021-03-29 20:40:40 -0400

    testexample

---
+++ src/hackkerrank/TestExample.java
@@ -0,0 +1,24 @@
+package hackkerrank;
+
+class Test2{
+	static int add(int a, int b) {
+		return (a+b);
+	}
+}
+
+public class Miscellaneous extends Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short s = 9;
+		System.out.println(add(s,10));
+		Float f1 = new Float("3.0");
+		int x = f1.intValue();
+		byte y = f1.byteValue();
+		double z = f1.doubleValue();
+		System.out.println(x+y+z);
+		int i = 010;
+		System.out.println(i);
+	}
+
+}
