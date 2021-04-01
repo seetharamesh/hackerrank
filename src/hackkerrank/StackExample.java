79bf382d3dade641c772acb41de01af2af92caaa
seetharamesh <seetharamesh@hotmail.com>
2021-04-01 19:33:59 -0400

    added a condition

--- src/hackkerrank/StackExample.java
+++ src/hackkerrank/StackExample.java
@@ -13,7 +13,11 @@ class StackExample{
 			//System.out.println(input.length());
 			Stack<Character> st = new Stack<>();
 			char ch = ' ';
+			if(input.length() % 2 != 0) System.out.println(false);//added this initial check if odd that means the strings are not closed
+			else
+			{
 			for(int i = 0; i < input.length(); i++) {
+				
 				ch = input.charAt(i);
 				if(!st.isEmpty())//if stack is not empty
 				{
@@ -25,18 +29,23 @@ class StackExample{
 					}
 					break;
 					
-					case ']': if(st.peek() == '[') st.pop();
+					case ']': if(st.peek() == '[') {
+						st.pop();
+					}
 					break;
 					
-					case ')': if(st.peek() == '(') st.pop();
+					case ')': if(st.peek() == '(') {
+						st.pop();
+					}
 					break;
 					
 					default: st.push(ch);
-					
+					break;
 					}
 				} else st.push(ch);
 			}
 			System.out.println(st.isEmpty());//if anything exists in stack that means brackets are not matching so it was not popped out
+			}
 		}
 		sc.close();
 		
