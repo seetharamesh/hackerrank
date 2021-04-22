c467084f7298fb326717a9d4bf642e01ec5c9e5d
seetharamesh <seetharamesh@hotmail.com>
2021-04-21 21:52:21 -0400

    recursion method

--- src/hackkerrank/BinaryTree.java
+++ src/hackkerrank/BinaryTree.java
@@ -21,17 +21,36 @@ class Node{
 public class BinaryTree { //this class will model the tree using Node class
 	//member of the class is Node
 	Node  root;
-	static boolean hasPathSum(Node n, int sum) {
-		//base case to exit
-		if(n == null)
-			return (sum ==0);
-		System.out.println(sum);
-		return hasPathSum(n.left, sum-n.data) || hasPathSum(n.right, sum-n.data) ;
+	static boolean hasPathSum(Node root, int s) {
+//		//base case to exit
+//		if(n == null)
+//			return (sum ==0);
+//		//System.out.println(n.data + " " + n.left + " " + n.right + " " + sum);
+//		return hasPathSum(n.left, sum-n.data) || hasPathSum(n.right, sum-n.data) ;
+		
+		boolean path=false; //declare boolean variable path
+		//base condition checking
+		if(root==null && s==0)
+			return true;
+
+		s-=root.data; //subtract current root value
+
+		//checking whether leaf node reached and remaining sum =0
+		if(s==0 && root.left==null && root.right==null) 
+			return true;
+		//recursively done for both subtrees
+		if(root.left != null){//for left subtree
+			path=path || hasPathSum(root.left, s);
+		}
+		if(root.right !=  null){//for right subtree
+			path= path || hasPathSum(root.right, s);
+		}
+		return path;
 		
 	}
 	
 	public static void main(String[] args) {
-		int sum = 20; //let's assume we need the path of tree that gives you sum of 26
+		int sum = 10; //let's assume we need the path of tree that gives you sum of 26
 		//Construct a tree here with node and child like in diagram below
 		/* REMEMBER: the term root takes you the root 10, NOT child's parent(like 8 or 2)
         10
