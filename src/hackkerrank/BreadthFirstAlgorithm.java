1df6790407b41624ad475cf8960e0df141a24a20
seetharamesh <seetharamesh@hotmail.com>
2021-04-25 14:46:44 -0400

    BFS algorithm

--- src/hackkerrank/BreadthFirstAlgorithm.java
+++ src/hackkerrank/BreadthFirstAlgorithm.java
@@ -30,12 +30,51 @@ public class BreadthFirstAlgorithm {
 	public void breadthFirstUsingQueue(Node n) {
 		
 	//yet to code
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(n);
+		n.visited = true;
+		
+		while(!queue.isEmpty()) {
+			Node element = queue.remove();
+			System.out.println(element.data);		
+			
+			List<Node> neighbors = element.getNeighbors(n);
+			for(int i = 0 ; i < neighbors.size(); i++) {
+				Node node = neighbors.get(i);
+				if(node != null && !node.visited) {
+					queue.add(node);
+					node.visited = true;
+				}
+			}
+		}
 		
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//create all vertices(nodes)
+		Node node40 = new Node(40);
+		Node node10 = new Node(10);
+		Node node20 = new Node(20);
+		Node node30 = new Node(30);
+		Node node60 = new Node(60);
+		Node node50 = new Node(50);
+		Node node70 = new Node(70);
+		//add neighbors to the nodes
+		node40.addNeighbors(node10);
+		node40.addNeighbors(node20);
+		node20.addNeighbors(node10);
+		node20.addNeighbors(node30);
+		node20.addNeighbors(node60);
+		node20.addNeighbors(node50);
+		node50.addNeighbors(node70);
+		node60.addNeighbors(node70);
+		node30.addNeighbors(node60);
+		node10.addNeighbors(node30);
 
+		System.out.println("Breadth Search Algorithm");
+		BreadthFirstAlgorithm bfa = new BreadthFirstAlgorithm();
+		//you can start the traversal from any node. Here we are using node 40
+		bfa.breadthFirstUsingQueue(node40);
 	}
 
 }
